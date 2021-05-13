139d7df1120504a67be1601912e60375bba0bef9
Serhii Orlenko <sorlenk@gmail.com>
2021-05-13 08:54:38 +0200

    JS: edit comments

--- js/index.js
+++ js/index.js
@@ -9,23 +9,23 @@ for (var i = 0; i < accordionTabHeaders.length; i++) {
 setActiveTab(accordionTabHeaders[0]);
 showContent(accordionTabHeaders[0]);
 // *** Highlighting active tab
-// Check if the active class is present
+// Check if the active class is present, and remove class
 function setActiveTab(tab) {
 	var activeTab = document.getElementById('accordion').querySelector('.active');
 	if (activeTab) {
 		activeTab.classList.remove('active');
 	};
-	// if not, add active class
+	// add active class
 	tab.classList.add('active');
 }
 // *** Content showing
-// Check if the active class is present
+// Check if the show class is present
 function showContent(content) {
 	var showContent = document.getElementById('accordion').querySelector('.show');
 	var contentToShow = document.getElementById('accordion').querySelector('.accordion__item--hiden');
 	if (showContent) {
 		showContent.classList.remove('show');
 	};
-	// if not, add active class
+	// add show class to show content
 	content.nextElementSibling.classList.add('show');
 }
